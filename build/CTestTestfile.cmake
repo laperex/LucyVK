c9bbd552de67d79ca60fc95c9a1e7e324fadf896
laperex <krishnadev7777@gmail.com>
2024-01-02 02:29:39 +0530

    someth

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/laperex/Programming/C++/LucyVK
-# Build directory: /home/laperex/Programming/C++/LucyVK/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
