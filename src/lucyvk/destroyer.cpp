13c9b491a864a0d7a325ff09e787ad9311ae6a86
laperex <krishnadev7777@gmail.com>
2025-01-04 00:38:57 +0530

    move complete

--- src/lucyvk/destroyer.cpp
+++ src/lucyvk/destroyer.cpp
@@ -4,230 +4,42 @@
 #include <cassert>
 #include <cstring>
 
-template <typename T>
-void _push(lvk_deletor_deque* destroyer, T data) {
-	static_assert(
-		std::is_same<T, VkCommandPool>() ||
-		std::is_same<T, VkPipelineLayout>() ||
-		std::is_same<T, VkPipeline>() ||
-		std::is_same<T, VkSwapchainKHR>() ||
-		std::is_same<T, VkSemaphore>() ||
-		std::is_same<T, VkFence>() ||
-		std::is_same<T, VkDescriptorSetLayout>() ||
-		std::is_same<T, VkDescriptorPool>() ||
-		std::is_same<T, VkFramebuffer>() ||
-		std::is_same<T, VkRenderPass>() ||
-		std::is_same<T, VkImageView>() ||
-		std::is_same<T, VkShaderModule>() ||
-		std::is_same<T, VkSampler>(),
-		"Error, Invalid Type!"
-	);
-
-	destroyer->delete_deque.push_back({
-		.data = { data },
-		.type = typeid(T).hash_code()
-	});
-}
-
-
-template <typename T>
-void _push(lvk_deletor_deque* destroyer, T data, VmaAllocation allocation) {
-	static_assert(
-		std::is_same<T, VkBuffer>() ||
-		std::is_same<T, VkImage>(),
-		"Error, Invalid Type!"
-	);
-
-	destroyer->delete_deque.push_back({
-		.data = { data, allocation },
-		.type = typeid(T).hash_code()
-	});
-}
-
-
-template <typename T, typename M>
-void _push(lvk_deletor_deque* destroyer, T* data, uint32_t data_count, M pool) {
-	static_assert(
-		(std::is_same<T, VkDescriptorSet>() && std::is_same<M, VkDescriptorPool>()) ||
-		(std::is_same<T, VkCommandBuffer>() && std::is_same<M, VkCommandPool>()),
-		"Error, Invalid Type!"
-	);
-
-	assert(data_count);
-
-	lvk_deletor_deque::delete_element element = {
-		.data = std::vector<void*>(data_count + 1),
-		.type = typeid(T).hash_code()
-	};
-
-	std::memcpy(element.data.data(), data, data_count * sizeof(T));
-	element.data[data_count] = pool;
-
-	destroyer->delete_deque.push_back(element);
-}
-
-
-void lvk_deletor_deque::push_function(VkCommandBuffer* command_buffers, uint32_t command_buffer_count, VkCommandPool command_pool) {
-	_push<VkCommandBuffer, VkCommandPool>(this, command_buffers, command_buffer_count, command_pool);
-}
-
-void lvk_deletor_deque::push_function(VkDescriptorSet* descriptor_sets, uint32_t descriptor_set_count, VkDescriptorPool descriptor_pool) {
-	_push<VkDescriptorSet, VkDescriptorPool>(this, descriptor_sets, descriptor_set_count, descriptor_pool);
-}
-
-
-void lvk_deletor_deque::push_function(VkBuffer buffer, VmaAllocation allocation) {
-	_push(this, buffer, allocation);
-}
-
-void lvk_deletor_deque::push_function(VkImage image, VmaAllocation allocation) {
-	_push(this, image, allocation);
-}
 
 const lvk_command_buffer& lvk_deletor_deque::push(const lvk_command_buffer& command_buffer, const lvk_command_pool& command_pool) {
-	push_function((VkCommandBuffer*)&command_buffer, 1, command_pool);
+	push_fn([=, this]() {
+		destroy((VkCommandBuffer*)&command_buffer, 1, command_pool);
+	});
 	return command_buffer;
 }
 
 const lvk_descriptor_set& lvk_deletor_deque::push(const lvk_descriptor_set& descriptor_set, const lvk_descriptor_pool& descriptor_pool) {
-	push_function((VkDescriptorSet*)&descriptor_set, 1, descriptor_pool);
+	push_fn([=, this]() {
+		destroy((VkDescriptorSet*)&descriptor_set, 1, descriptor_pool);
+	});
 	return descriptor_set;
 }
 
-void lvk_deletor_deque::push_function(VkCommandPool command_pool) {
-	_push(this, command_pool);
-}
-
-void lvk_deletor_deque::push_function(VkPipelineLayout pipeline_layout) {
-	_push(this, pipeline_layout);
-}
-
-void lvk_deletor_deque::push_function(VkPipeline pipeline) {
-	_push(this, pipeline);
-}
-
-void lvk_deletor_deque::push_function(VkSwapchainKHR swapchain) {
-	_push(this, swapchain);
-}
-
-void lvk_deletor_deque::push_function(VkSemaphore semaphore) {
-	_push(this, semaphore);
-}
-
-void lvk_deletor_deque::push_function(VkFence fence) {
-	_push(this, fence);
-}
-
-void lvk_deletor_deque::push_function(VkDescriptorSetLayout descriptor_set_layout) {
-	_push(this, descriptor_set_layout);
-}
-
-void lvk_deletor_deque::push_function(VkDescriptorPool descriptor_pool) {
-	_push(this, descriptor_pool);
-}
-
-void lvk_deletor_deque::push_function(VkFramebuffer framebuffer) {
-	_push(this, framebuffer);
-}
-
-void lvk_deletor_deque::push_function(VkRenderPass render_pass) {
-	_push(this, render_pass);
-}
-
-void lvk_deletor_deque::push_function(VkImageView image_view) {
-	_push(this, image_view);
-}
-
-void lvk_deletor_deque::push_function(VkShaderModule shader_module) {
-	_push(this, shader_module);
-}
-
-void lvk_deletor_deque::push_function(VkSampler sampler) {
-	_push(this, sampler);
-}
-
-
 const lvk_buffer& lvk_deletor_deque::push(const lvk_buffer& buffer) {
-	_push(this, buffer._buffer, buffer._allocation);
+	push_fn([=, this]() {
+		destroy((VkBuffer)buffer._buffer, (VmaAllocation)buffer._allocation);
+	});
 	return buffer;
 }
 
 const lvk_image& lvk_deletor_deque::push(const lvk_image& image) {
-	_push(this, image._image, image._allocation);
+	push_fn([=, this]() {
+		destroy((VkImage)image._image, (VmaAllocation)image._allocation);
+	});
 	return image;
 }
 
 void lvk_deletor_deque::push_fn(std::function<void()>&& function) {
-	delete_deque.push_back({
-		.data = { (void*)&function },
-		.type = typeid(std::function<void()>).hash_code()
-	});
+	delete_deque.push_back(function);
 }
 
-// move this to a function
 void lvk_deletor_deque::flush() {
-	int i = 0;
 	for (auto element = delete_deque.rbegin(); element != delete_deque.rend(); element++) {
-		if (element->type == typeid(std::function<void()>).hash_code()) {
-			dloggln("ssadadsasd");
-			void_lambda_function fn = (void_lambda_function)element->data[0];
-			fn();
-		}
-		if (element->type == typeid(VkCommandPool).hash_code()) {
-			destroy(static_cast<VkCommandPool>(element->data[0]));
-		}
-		if (element->type == typeid(VkPipelineLayout).hash_code()) {
-			destroy(static_cast<VkPipelineLayout>(element->data[0]));
-		}
-		if (element->type == typeid(VkPipeline).hash_code()) {
-			destroy(static_cast<VkPipeline>(element->data[0]));
-		}
-		if (element->type == typeid(VkSwapchainKHR).hash_code()) {
-			destroy(static_cast<VkSwapchainKHR>(element->data[0]));
-		}
-		if (element->type == typeid(VkSemaphore).hash_code()) {
-			destroy(static_cast<VkSemaphore>(element->data[0]));
-		}
-		if (element->type == typeid(VkFence).hash_code()) {
-			destroy(static_cast<VkFence>(element->data[0]));
-		}
-		if (element->type == typeid(VkDescriptorSetLayout).hash_code()) {
-			destroy(static_cast<VkDescriptorSetLayout>(element->data[0]));
-		}
-		if (element->type == typeid(VkDescriptorPool).hash_code()) {
-			destroy(static_cast<VkDescriptorPool>(element->data[0]));
-		}
-		if (element->type == typeid(VkFramebuffer).hash_code()) {
-			destroy(static_cast<VkFramebuffer>(element->data[0]));
-		}
-		if (element->type == typeid(VkRenderPass).hash_code()) {
-			destroy(static_cast<VkRenderPass>(element->data[0]));
-		}
-		if (element->type == typeid(VkImageView).hash_code()) {
-			destroy(static_cast<VkImageView>(element->data[0]));
-		}
-		if (element->type == typeid(VkShaderModule).hash_code()) {
-			destroy(static_cast<VkShaderModule>(element->data[0]));
-		}
-		if (element->type == typeid(VkSampler).hash_code()) {
-			destroy(static_cast<VkSampler>(element->data[0]));
-		}
-
-		if (element->type == typeid(VkCommandBuffer).hash_code()) {
-			destroy((VkCommandBuffer*)element->data.data(), element->data.size() - 1, (VkCommandPool)element->data.back());
-		}
-		
-		if (element->type == typeid(VkDescriptorSet).hash_code()) {
-			destroy((VkDescriptorSet*)element->data.data(), element->data.size() - 1, (VkDescriptorPool)element->data.back());
-		}
-
-		if (element->type == typeid(VkBuffer).hash_code()) {
-			destroy((VkBuffer)element->data[0], (VmaAllocation)element->data[1]);
-		}
-		
-		if (element->type == typeid(VkImage).hash_code()) {
-			destroy((VkImage)element->data[0], (VmaAllocation)element->data[1]);
-		}
+		(*element)();
 	}
 
 	delete_deque.clear();
@@ -237,7 +49,6 @@ void lvk_deletor_deque::flush() {
 void lvk_deletor_deque::destroy(VkCommandPool command_pool) {
 	vkDestroyCommandPool(device, command_pool, VK_NULL_HANDLE);
 	dloggln("DESTROYED \t", command_pool, "\t [CommandPool]");
-	// deletor.push_function()
 }
 
 
--- src/lucyvk/destroyer.h
+++ src/lucyvk/destroyer.h
@@ -53,65 +53,37 @@
 
 
 struct lvk_deletor_deque {
-	struct delete_element {
-		std::vector<void*> data = {};
-		std::size_t type = 0;
-	};
-	
-	typedef std::function<void()> void_lambda_function;
-
-	std::deque<delete_element> delete_deque;
+	std::deque<std::function<void()>> delete_deque;
 	VkDevice device;
 	VmaAllocator allocator;
 
-private:
-	void push_function(VkCommandPool command_pool);
-	void push_function(VkPipelineLayout pipeline_layout);
-	void push_function(VkPipeline pipeline);
-	void push_function(VkSwapchainKHR swapchain);
-	void push_function(VkSemaphore semaphore);
-	void push_function(VkFence fence);
-	void push_function(VkDescriptorSetLayout descriptor_set_layout);
-	void push_function(VkDescriptorPool descriptor_pool);
-	void push_function(VkFramebuffer framebuffer);
-	void push_function(VkRenderPass render_pass);
-	void push_function(VkImageView image_view);
-	void push_function(VkShaderModule shader_module);
-	void push_function(VkSampler sampler);
-	void push_function(VkCommandBuffer* command_buffers, uint32_t command_buffer_count, VkCommandPool command_pool);
-	void push_function(VkDescriptorSet* descriptor_sets, uint32_t descriptor_set_count, VkDescriptorPool descriptor_pool);
-	// void push_function(VkCommandBuffer command_buffers, uint32_t command_buffer_count, VkCommandPool command_pool);
-
-	void push_function(VkBuffer buffer, VmaAllocation allocation);
-	void push_function(VkImage buffer, VmaAllocation allocation);
-
-public:
 	const lvk_command_buffer& push(const lvk_command_buffer& command_buffer, const lvk_command_pool& command_pool);
 	const lvk_descriptor_set& push(const lvk_descriptor_set& descriptor_set, const lvk_descriptor_pool& descriptor_pool);
 	const lvk_buffer& push(const lvk_buffer& buffer);
 	const lvk_image& push(const lvk_image& image);
+	
+	
 
-	template <typename T>
-	const T& push(const T& handle) {
-		static_assert(
-			std::is_same<T, lvk_command_buffer>() ||
-			std::is_same<T, lvk_command_pool>() ||
-			std::is_same<T, lvk_pipeline_layout>() ||
-			std::is_same<T, lvk_pipeline>() ||
-			std::is_same<T, lvk_semaphore>() ||
-			std::is_same<T, lvk_fence>() ||
-			std::is_same<T, lvk_descriptor_set_layout>() ||
-			std::is_same<T, lvk_descriptor_pool>() ||
-			std::is_same<T, lvk_framebuffer>() ||
-			std::is_same<T, lvk_render_pass>() ||
-			std::is_same<T, lvk_image_view>() ||
-			std::is_same<T, lvk_shader_module>() ||
-			std::is_same<T, lvk_sampler>(),
-			"Error, Invalid Type!"
-		);
-		push_function(handle);
-		return handle;
-	}
+	#define PUSH_DEF(T, M) \
+		const T& push(const T& handle) { \
+			push_fn([=, this]() { \
+				destroy(static_cast<M>(handle)); \
+			}); \
+			return handle; \
+		}
+
+	PUSH_DEF(lvk_command_pool, VkCommandPool);
+	PUSH_DEF(lvk_pipeline_layout, VkPipelineLayout);
+	PUSH_DEF(lvk_pipeline, VkPipeline);
+	PUSH_DEF(lvk_semaphore, VkSemaphore);
+	PUSH_DEF(lvk_fence, VkFence);
+	PUSH_DEF(lvk_descriptor_set_layout, VkDescriptorSetLayout);
+	PUSH_DEF(lvk_descriptor_pool, VkDescriptorPool);
+	PUSH_DEF(lvk_framebuffer, VkFramebuffer);
+	PUSH_DEF(lvk_render_pass, VkRenderPass);
+	PUSH_DEF(lvk_image_view, VkImageView);
+	PUSH_DEF(lvk_shader_module, VkShaderModule);
+	PUSH_DEF(lvk_sampler, VkSampler);
 	
 	void push_fn(std::function<void()>&& function);
 	
